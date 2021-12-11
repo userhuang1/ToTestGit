6c0572e87e655a665cbbb3ece8ef34ea71337a0c
huangxiaobo <huangxiaobo@nowcoder.com>
2021-12-11 20:29:36 +0800

    feat(添加个小东西): 测试添加（git使用）(这里我还两个提交放到一起)

--- untitled/src/main/java/com/hxb/UserController.java
+++ untitled/src/main/java/com/hxb/UserController.java
@@ -5,5 +5,7 @@ import java.sql.SQLOutput;
 public class UserController {
     public static void main(String[] args) {
         System.out.println("Hello World!");
+        System.out.println("bye java");
+        System.out.println("i said wrong");
     }
 }
