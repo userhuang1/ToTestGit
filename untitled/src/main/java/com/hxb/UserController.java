9724d9fdc03b89cf605e53a0dd2d4158ab44b03e
huangxiaobo <huangxiaobo@nowcoder.com>
2021-12-11 20:30:50 +0800

    feat(添加个小东西2): 测试添加（git使用）

--- untitled/src/main/java/com/hxb/UserController.java
+++ untitled/src/main/java/com/hxb/UserController.java
@@ -6,5 +6,6 @@ public class UserController {
     public static void main(String[] args) {
         System.out.println("Hello World!");
         System.out.println("bye java");
+        System.out.println("i said wrong");
     }
 }
