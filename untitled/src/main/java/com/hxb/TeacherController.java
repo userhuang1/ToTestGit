6801c7e961269a26f7ce21a1f6172b9bb228be24
huangxiaobo <huangxiaobo@nowcoder.com>
2021-12-27 15:05:59 +0800

    test2

--- untitled/src/main/java/com/hxb/TeacherController.java
+++ untitled/src/main/java/com/hxb/TeacherController.java
@@ -3,5 +3,6 @@ package com.hxb;
 public class TeacherController {
     public static void main(String[] args) {
         System.out.println("2021年12月27日15:05:18");
+        System.out.println("2021年12月27日15:05:50");
     }
 }
